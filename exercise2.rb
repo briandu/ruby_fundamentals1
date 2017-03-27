6510d6b2f226d1479a8e0aee78ec63f8420cb8a4
BRIAN DU <brian.bh.du@gmail.com>
2017-03-27 15:16:08 -0400

    Exercise 3

---
+++ exercise2.rb
@@ -0,0 +1,23 @@
+puts (55.00 * 0.13)
+puts "String + 55"
+puts "The answer for 45628 multiplied by 7839 is #{45628 * 7839}"
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+
+my_variable = 'my_variable is now this string'
+
+name = "Sandra"
+greeting = "Hello #{name}! It's good to see you again."
+mission = "Your mission, should you choose to accept it..."
+
+puts greeting + mission
+
+amount = 20
+new_amount = amount
+new_amount # 20
+amount = "twenty"
+amount # "twenty"
+new_amount # 20
+
+
+
+first_amount, second_amount, third_amount = 10, 20, 30
